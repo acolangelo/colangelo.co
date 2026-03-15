50c78f1f8c20e882a5c755ebc5a33c160888d3fa
Anthony Colangelo <anthony@pineworks.co>
2026-03-15 09:01:12 -0400

    Reverting to Colangelo orange

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <main class="text-[18vw]">
-	<h1 class="mb-[13vw]"><a href={base || '/'} class="hover:text-orange-600">Colangelo Studios</a></h1>
+	<h1 class="mb-[13vw]"><a href={base || '/'} class="hover:text-[#CE8000]">Colangelo Studios</a></h1>
 	<p class="mb-[13vw]"><a href="https://pineworks.co" class="hover:text-[#5F7D6F] dark:hover:text-[#7da695]">Pine Works</a></p>
 	<p class="mb-[13vw]"><a href="https://mainenginecutoff.com" class="hover:text-[#3e7493] dark:hover:text-[#66a3c6]">Main Engine Cut Off</a></p>
 	<p class="mb-[13vw]"><a href="https://offnom.com" class="hover:text-[#7b943c]">Off-Nominal</a></p>
 </main>
 
 <footer class="mt-[13vw] text-lg">
-	<p>From the studio of <a href="https://acolangelo.com" class="underline hover:text-orange-600">Anthony Colangelo</a></p>
+	<p>From the studio of <a href="https://acolangelo.com" class="underline hover:text-[#CE8000]">Anthony Colangelo</a></p>
 </footer>
