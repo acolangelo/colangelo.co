458f6ce1f30eefe63a37fa45aa8a6d9040898131
Anthony Colangelo <anthony@pineworks.co>
2026-03-15 23:45:24 -0400

    Simpler “From the studio of…” page

---
+++ docs/_app/immutable/entry/start.DTnrcT5v.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CNXOC7C2.js";import{x as t}from"../chunks/TTVhv-NM.js";export{t as load_css,r as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { base } from '$app/paths';
 </script>
 
-<main class="text-[18vw]">
+<!-- <main class="text-[18vw]">
 	<h1 class="mb-[13vw]"><a href={base || '/'} class="hover:text-[#CE8000]">Colangelo Studios</a></h1>
 	<p class="mb-[13vw]"><a href="https://pineworks.co" class="hover:text-[#5F7D6F] dark:hover:text-[#7da695]">Pine Works</a></p>
 	<p class="mb-[13vw]"><a href="https://mainenginecutoff.com" class="hover:text-[#3e7493] dark:hover:text-[#66a3c6]">Main Engine Cut Off</a></p>
@@ -11,4 +11,14 @@
 
 <footer class="mt-[13vw] text-lg">
 	<p>From the studio of <a href="https://acolangelo.com" class="underline hover:text-[#CE8000]">Anthony Colangelo</a></p>
-</footer>
+</footer> -->
+
+
+<main class="h-screen flex flex-col p-8 md:p-16">
+	<h1 class="flex-1 max-w-[13rem] md:max-w-[15rem] text-xl">From the studio of <a href="https://acolangelo.com" class="md:text-3xl hover:text-[#CE8000]">Anthony Colangelo</a></h1>
+	<ul class="text-lg flex flex-col gap-1 md:flex-row md:gap-8">
+		<li><a href="https://pineworks.co" class="hover:text-[#5F7D6F] dark:hover:text-[#7da695]">Pine Works</a></li>
+		<li><a href="https://mainenginecutoff.com" class="hover:text-[#3e7493] dark:hover:text-[#66a3c6]">Main Engine Cut Off</a></li>
+		<li><a href="https://offnom.com" class="hover:text-[#7b943c]">Off-Nominal</a></li>
+	</ul>
+</main>
